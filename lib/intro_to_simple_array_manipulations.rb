e161fb71a798f98d69e272be5fb3270e632868ef
Natalie Taktachev <ntaktach@gmail.com>
2019-07-10 01:01:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,8 +19,8 @@ def using_shift(my_favorite_cities)
 im_so_over_this_city = my_favorite_cities.shift
 end
 
-def shift_with_args(ice_cream_brands, brands_removed)
-  ice_cream_brands.shift(brands_removed)
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
 end
 
 def using_concat(my_favorite_things, more_favs)
